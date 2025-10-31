b314d04ab619092faa32f8b784a96078b9d46e57
MartaCEI <guecolmar@alumnos.cei.es>
2025-10-31 20:24:42 +0100

    home page update

--- src/components/homes/HomeCard.jsx
+++ src/components/homes/HomeCard.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const HomeCard = ({title, link, button, image}) => {
     const VITE_FRONTEND_IMG = import.meta.env.VITE_FRONTEND_IMG;
     return (
-        <div className="home-grid">
+        <article className="home-article">
             <div className="home-containers">
                 <img
-                    className="top-img"
+                    className="home-img"
                     src={`${VITE_FRONTEND_IMG}/${image}`}
                     alt={button || "imagen"}
                 />
@@ -15,7 +15,7 @@ const HomeCard = ({title, link, button, image}) => {
                 <h2 className="home-h2">{title}</h2>
                 <Link to={link} className="home-button">{button}</Link>
             </div>
-        </div>
+        </article>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,41 +2,28 @@ import TopPictures from "@/components/TopPictures";
 import { Link } from "react-router-dom";
 import { usePageInfo } from "@/hooks/usePageInfo";
 import { useEffect } from "react";
+import HomeCard from "../components/homes/homeCard";
 
 const Home = () => {
-        const { pageInfo, fetchPageInfo, } = usePageInfo();
+    const { pageInfo, fetchPageInfo, fetchHomeInfo, homeInfo} = usePageInfo();
 
-        useEffect(() => {
-            fetchPageInfo("home");
-        }, []);
+    useEffect(() => {
+        fetchPageInfo("home");
+        fetchHomeInfo();
+    }, []);
 
     return (
         <>
             {pageInfo && <TopPictures pageInfo={pageInfo} />}
             <section className="home-flex">
-                <div className="home-grid">
-                    <div className="home-containers">
-                        <img src="https://picsum.photos/500/700" alt="" />
-                    </div>
-                    <div className="home-containers">
-                        <h2 className="home-h2">Nuestro Menú</h2>
-                        <Link to="/menu" className="home-button">Menú</Link>
-                    </div>
-                    <div className="home-containers">
-                        <h2 className="home-h2">Para llevar</h2>
-                        <Link to="/takeout" className="home-button">Takeout</Link>
-                    </div>
-                    <div className="home-containers">
-                        <img src="https://picsum.photos/500/700" alt="" />
-                    </div>
-                    <div className="home-containers">
-                        <img src="https://picsum.photos/500/700" alt="" />
-                    </div>
-                    <div className="home-containers">
-                        <h2 className="home-h2">eventos</h2>
-                        <Link to="/events" className="home-button">eventos</Link>
-                    </div>
-                </div>
+                {
+                    homeInfo && homeInfo.map((info, index) => 
+                        < HomeCard key={index} {...info} />
+                    )
+                }
+
+
+                
             </section>
         </>
     );
